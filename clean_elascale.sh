50018adc94ca01a867b5f144f110623d7f208c3c
rajsimmanravi <rajsimmanr@gmail.com>
2018-04-15 19:10:01 -0400

    Fixed autoscaler scripts and added them to deploy/clean up scripts

--- clean_elascale.sh
+++ clean_elascale.sh
@@ -5,7 +5,7 @@ SCRIPTS_DIR="/home/ubuntu/Elascale_secure"
 #remove the stacks and services first
 sudo docker stack rm EK_monitor
 sudo docker stack rm beats
-sudo docker stack rm elascale  
+sudo $SCRIPTS_DIR/./stop-autoscaler 
 
 #Now, remove the worker node
 sudo docker-machine rm -y iot-monitor
